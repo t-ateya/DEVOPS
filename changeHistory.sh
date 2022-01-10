7cf1e04dd0e089b4f77225b9d3bfd6324784872e
ateya <ateyaterence@gmail.com>
2022-01-10 16:38:07 -0600

    added new files

---
+++ changeHistory.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+git filter-branch --env-filter '
+WRONG_EMAIL="anshulc55@gmail.com"
+NEW_NAME="Terence Ateya"
+NEW_EMAIL="ateyaterence@gmail.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$WRONG_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$NEW_NAME"
+    export GIT_COMMITTER_EMAIL="$NEW_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$WRONG_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$NEW_NAME"
+    export GIT_AUTHOR_EMAIL="$NEW_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
